456e2626af6b6f57012da899b6a631d1bbb45218
RayirthDinesh <91096595+RayirthDinesh@users.noreply.github.com>
2024-09-20 12:21:12 -0700

    ASLify React Native App - Speech or Text to ASL (American Sign Language)

---
+++ metro.config.js
@@ -0,0 +1,18 @@
+const { getDefaultConfig } = require('metro-config');
+
+
+
+module.exports = (async () => {
+  const {
+    resolver: { sourceExts, assetExts },
+  } = await getDefaultConfig();
+  return {
+    resolver: {
+      sourceExts: [...sourceExts, 'svg'],
+      assetExts: assetExts.filter(ext => ext !== 'svg'),
+    },
+    transformer: {
+      babelTransformerPath: require.resolve('react-native-svg-transformer'),
+    },
+  };
+})();
